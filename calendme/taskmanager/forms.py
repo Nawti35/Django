3f57a8b37e9c2b33ad7e8d52d25339858dd3f311
Alexis Le Penven <alexis.lepenven@gmail.com>
2020-05-07 12:25:53 +0200

    Modification de l'aspect des forms et modification du footer

--- calendme/taskmanager/forms.py
+++ calendme/taskmanager/forms.py
@@ -6,6 +6,17 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs): #Défini la mise en forme des champs à l'aide de Bootstrap
+        super().__init__(*args, **kwargs)
+        self.fields['project'].widget.attrs.update({'class': 'form-control'})
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['assignee'].widget.attrs.update({'class': 'form-control'})
+        self.fields['start_date'].widget.attrs.update({'class': 'form-control'}) #Les dates doivent être retrées dans le format jj/mm/aaaa
+        self.fields['due_date'].widget.attrs.update({'class': 'form-control'})
+        self.fields['priority'].widget.attrs.update({'class': 'form-control'})
+        self.fields['status'].widget.attrs.update({'class': 'form-control'})
+
 class JournalForm(forms.ModelForm):
     class Meta:
         model = Journal
